43f7d8fc586ff8772e01334015a49f4c0718ec9d
dami0806 <dami0804@naver.com>
2024-09-03 14:59:35 +0900

    init
    project setting

---
+++ src/main/java/com/sparta/preonboardingbackendcourse/PreonboardingBackendCourseApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.preonboardingbackendcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreonboardingBackendCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreonboardingBackendCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/preonboardingbackendcourse/PreonboardingBackendCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.preonboardingbackendcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreonboardingBackendCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
